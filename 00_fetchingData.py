8b211b01807b574048d427bb8d402460d449dc11
Jiten Rai <raijiten96@gmail.com>
2024-12-06 23:47:43 +0545

    webscraping done (success)

--- 00_fetchingData.py
+++ 00_fetchingData.py
@@ -1,10 +1,5 @@
 import requests
 
-proxies = {
-    "http": "http://49.0.2.242:8090",
-    "https": "https://192.53.123.54:8080"
-}
-
 def fetchAndSave(url, path):
     r = requests.get(url)
     with open(path, "w", encoding='utf-8') as f:
--- webScraping.py
+++ webScraping.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 proxies = {
-    "http": "http://154.16.146.44:80",
-    
+    "http": "http://154.16.146.44:80"
 }
 
 url = 'https://texascollege.edu.np/bit/'
+#THE COMMENTED OUT PART WAS DONE FOR PRACTICE PURPOSE
 # url = 'https://www.amazon.com/s?k=iphones'
 # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
@@ -17,6 +17,34 @@ r = requests.get(url, proxies=proxies)
 soup = BeautifulSoup(r.text, 'html.parser')
 # print(soup.prettify())
 
+# divs = soup.find(class_="accordion-item")
+# print(divs)
+
+# ths = soup.select('th')
+# for th in ths:
+#     print(th.string)
+
+# tds = soup.select('td')
+# for td in tds:
+#     print(td.string)
+
+data = []
+accordian_items = soup.find_all('div', class_="accordion-item")
+
+for item in accordian_items:
+    table_rows = item.find('tbody').find_all('tr')
+    for row in table_rows:
+        cells = row.find_all('td')
+        course_name = cells[1].get_text(strip=True)
+        course_code = cells[2].get_text(strip=True)
+        data.append([course_name, course_code])
+
+df = pd.DataFrame(data, columns=['Course Name', 'Course Code'])
+df.to_csv('course_code.csv', index=False)
+print('Data retrieved!')
+
+# print(soup.prettify())
+
 # spans = soup.select('span.a-size-medium.a-color-base.a-text-normal')
 # for span in spans:
 #     print(span.string)
@@ -24,26 +52,3 @@ soup = BeautifulSoup(r.text, 'html.parser')
 # prices = soup.select('span.a-price')
 # for price in prices:
 #     print(price.find('span').get_text())
-
-
-#THIS PART WAS WORKED ON TEXAS WEBSITE
-# print(soup.prettify())
-
-# divs = soup.find(class_="accordion-item")
-# print(divs)
-
-data = {'Title':[], 'Data':[]}
-
-ths = soup.select('th')
-for th in ths:
-    print(th.string)
-    data['Title'].append(th.string)
-
-tds = soup.select('td')
-for td in tds:
-    print(td.string)
-    data['Data'].append(td.string)
-
-#THIS DIDN'T WORK
-# df = pd.DataFrame.from_dict(data)
-# df.to_csv('data.csv', index=False)
